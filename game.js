6380195e3c7c0916b1c74161a7d744156635292b
Jasmina <126290779+BalasJasminaAndra@users.noreply.github.com>
2023-05-11 20:16:01 +0300

    Add player inputs

--- game.js
+++ game.js
@@ -2,4 +2,48 @@ class Player {
   constructor(name) {
     this.name = name;
   }
+
+  setName(name) {
+    this.name = name;
+  }
 }
+
+let player1;
+let player2;
+
+function setup() {
+  createCanvas(800, 740);
+  player1 = new Player("Mutulică");
+  player2 = new Player("Mutulica");
+  createInputs();
+}
+
+function createInputs() {
+  let inputName1 = createInput("");
+  inputName1.position(650, 10);
+  inputName1.size(150);
+  inputName1.input(setName1);
+  inputName1.value("player1");
+
+  let inputName2 = createInput("");
+  inputName2.position(650, 650);
+  inputName2.size(150);
+  inputName2.input(setName2);
+  inputName2.value("player2");
+}
+
+function setName1() {
+  player1.setName(this.value());
+}
+
+function setName2() {
+  player2.setName(this.value());
+}
+
+function draw() {
+  background(220);
+  text(player1.name, 650, 50);
+  text(player2.name, 650, 690);
+}
+
+function playerName() {}
