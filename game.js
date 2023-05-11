f30f51e7cc3f69fed093d6b7cabd92f3c3adebaf
Jasmina <126290779+BalasJasminaAndra@users.noreply.github.com>
2023-05-11 21:02:10 +0300

    crearea tablei de joc

--- game.js
+++ game.js
@@ -3,19 +3,55 @@ class Player {
     this.name = name;
   }
 
+  //schimb nume
   setName(name) {
     this.name = name;
   }
 }
 
+class Table {
+  constructor(rows, cols) {
+    this.rows = rows;
+    this.cols = cols;
+    this.table = [];
+
+    for (let i = 0; i < this.rows; i++) {
+      this.table[i] = [];
+      for (let j = 0; j < this.cols; j++) {
+        this.table[i][j] = 0;
+      }
+    }
+  }
+
+  //afisarea tablei
+  draw(x, y) {
+    let cellSize = 40;
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        if (i % 2 == 0 && j % 2 == 0) {
+          let cellX = x + j * 0.7 * cellSize;
+          let cellY = y + i * 0.7 * cellSize;
+
+          fill("rgba(0,255,0, 0.25)");
+          stroke(0);
+          rect(cellX, cellY, cellSize, cellSize);
+        }
+      }
+    }
+  }
+}
+
+//initializare jucatorii
 let player1;
 let player2;
 
 function setup() {
-  createCanvas(800, 740);
+  createCanvas(800, 700);
   player1 = new Player("Mutulică");
   player2 = new Player("Mutulica");
   createInputs();
+  table = new Table(17, 17);
 }
 
 function createInputs() {
@@ -32,6 +68,15 @@ function createInputs() {
   inputName2.value("player2");
 }
 
+function draw() {
+  background("pink");
+  textSize(32);
+  fill("black");
+  text(player1.name, 650, 50);
+  text(player2.name, 650, 690);
+  table.draw(50, 100);
+}
+
 function setName1() {
   player1.setName(this.value());
 }
@@ -39,11 +84,3 @@ function setName1() {
 function setName2() {
   player2.setName(this.value());
 }
-
-function draw() {
-  background(220);
-  text(player1.name, 650, 50);
-  text(player2.name, 650, 690);
-}
-
-function playerName() {}
